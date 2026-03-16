6c337b526a35ec2fcc01279368ebf4418e1b6a9e
Marcus <omarcusvinicius96@gmail.com>
2026-03-16 15:16:32 -0300

    organizando as pastas

---
+++ exe1/agent.py
@@ -0,0 +1,15 @@
+class Agent(Thing):
+    def __init__(self, program=None):
+        self.alive = True
+        self.bump = False
+        self.holding = []
+        self.performance = 0
+        # Use callable() for a more robust check
+        if program is None or not callable(program):
+            def program(percept):
+                # This is the default interactive program if no valid program is provided
+                return eval(input('Percept={}; action? '.format(percept)))
+        self.program = program
+
+    def can_grab(self, thing):
+        return False
---
+++ exe1/environment.py
@@ -0,0 +1,50 @@
+class Environment:
+
+    def __init__(self):
+        self.things = []
+        self.agents = []
+
+    def percept(self, agent):
+        raise NotImplementedError
+
+    def execute_action(self, agent, action):
+        raise NotImplementedError
+
+    def default_location(self, thing):
+        return None
+
+    def exogenous_change(self):
+        pass
+
+    def is_done(self):
+        return not any(agent.is_alive() for agent in self.agents)
+
+    def step(self):
+        if not self.is_done():
+            actions = []
+            for agent in self.agents:
+                if agent.alive:
+                    actions.append(agent.program(self.percept(agent)))
+                else:
+                    actions.append("")
+            for (agent, action) in zip(self.agents, actions):
+                self.execute_action(agent, action)
+            self.exogenous_change()
+
+    def run(self, steps=1000):
+        for step in range(steps):
+            if self.is_done():
+                return
+            self.step()
+
+    def add_thing(self, thing, location=None):
+        if not isinstance(thing, Thing):
+            thing = Agent(thing)
+        if thing in self.things:
+            print("Can't add the same thing twice")
+        else:
+            thing.location = location if location is not None else self.default_location(thing)
+            self.things.append(thing)
+            if isinstance(thing, Agent):
+                thing.performance = 0
+                self.agents.append(thing)
